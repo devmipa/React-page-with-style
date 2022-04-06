c5955e4c1b2d1f06716f197c68445949a4cccbb5
devmipa <devmipa@gmail.com>
2022-04-06 11:07:19 -0400

    Create React-page-with-style

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Main (){
+    return(
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Is declarative instead imperative</li>
+                <li>Is a ligthweight library</li>
+                <li>Lets you to divide the UI into components</li>
+                <li>Is maintanable</li>
+                <li>Use pure functions</li>
+                <li>Power thousands of enterprices sites, including apps</li>
+                <li>I enjoy learning React</li>
+            </ul>
+        </main>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NavBar() {
+    return(
+        <nav>
+            <img src='./logo192.png' />
+            <h3>React-Facts</h3>
+            <h4>React-Project</h4>
+        </nav>
+    )
+}
